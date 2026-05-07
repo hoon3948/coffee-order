d377f31780315ac65649bca70bed8f41a9be1bc1
hoon3948 <robot3949@gmail.com>
2026-05-07 16:27:01 +0900

    fix:컨플릭트로 꼬인 내용 수정

--- src/test/java/kr/spartaclub/coffeeorder/CoffeeOrderApplicationTests.java
+++ src/test/java/kr/spartaclub/coffeeorder/CoffeeOrderApplicationTests.java
@@ -3,12 +3,10 @@ package kr.spartaclub.coffeeorder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import kr.spartaclub.coffeeorder.config.TestConfig;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @Import(TestConfig.class)
 class CoffeeOrderApplicationTests {
 
