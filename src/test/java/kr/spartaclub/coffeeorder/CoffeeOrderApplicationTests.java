4bb9135e4133ecd2c9138b81d8b93dc412448772
hoon3948 <robot3949@gmail.com>
2026-05-07 16:12:09 +0900

    fix:test 빌드 실패 수정

--- src/test/java/kr/spartaclub/coffeeorder/CoffeeOrderApplicationTests.java
+++ src/test/java/kr/spartaclub/coffeeorder/CoffeeOrderApplicationTests.java
@@ -2,8 +2,14 @@ package kr.spartaclub.coffeeorder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import kr.spartaclub.coffeeorder.config.TestConfig;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestConfig.class)
 class CoffeeOrderApplicationTests {
 
     @Test
